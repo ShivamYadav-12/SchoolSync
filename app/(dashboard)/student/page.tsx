eddd380feaa3eca04d77e2e00f48e04d3e8699c5
ShivamYadav-12 <shivamyadav99512@gmail.com>
2024-10-04 23:55:49 +0530

    student UI calendar

--- app/(dashboard)/student/page.tsx
+++ app/(dashboard)/student/page.tsx
@@ -1,7 +1,22 @@
+import Announcements from "@/app/components/Announcements";
+import BigCalendar from "@/app/components/BigCalendar";
+import EventCalendar from "@/app/components/EventCalendar";
+
 const Student = ()=>{
     return(
-        <div>
-            studentpage
+        <div className="p-4 flex gap-4 flex-col xl:flex-row">
+        <div className="w-full xl:w-2/3">
+        <div className="bg-white h-full rounded-md p-4">
+        <h1 className="font-semibold text-xl">Schedule(4A)</h1>
+        <BigCalendar/>
+
+        </div>
+        </div>
+        <div className="w-full xl:w-1/3">
+        <EventCalendar/>
+        <Announcements/>
+        
+        </div>
         </div>
     )
 }
